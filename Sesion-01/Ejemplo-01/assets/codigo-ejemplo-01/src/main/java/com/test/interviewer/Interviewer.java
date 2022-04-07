e5759d65a5e825556d8386662fb250c907ba943b
Beto <49173908+beduExpert@users.noreply.github.com>
2022-04-07 11:53:48 -0500

    Add files via upload

---
+++ Sesion-01/Ejemplo-01/assets/codigo-ejemplo-01/src/main/java/com/test/interviewer/Interviewer.java
@@ -0,0 +1,70 @@
+package com.test.interviewer;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Interviewer implements Serializable {
+    static ArrayList<Interviewer> data;
+
+    int id;
+    String name;
+    String lastName;
+    String email;
+    Boolean isActive;
+
+    public Interviewer(
+            String name,
+            String lastName,
+            String email,
+            Boolean isActive
+    ) {
+        this.id = data.size() + 1;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.isActive = isActive;
+    }
+
+    public Interviewer add() {
+        data.add(this);
+        return this;
+    }
+
+    public void save(
+            String name,
+            String lastName,
+            String email,
+            Boolean isActive
+    ) {
+        if (!name.equals(""))
+            this.name = name;
+
+        if (!lastName.equals(""))
+            this.lastName = lastName;
+
+        if (!email.equals(""))
+            this.email = email;
+
+        this.isActive = isActive;
+
+        data.add(this);
+    }
+
+    public static Interviewer getByEmail(String email) {
+        for (Interviewer interviewer: data) {
+            if (interviewer.email.equals(email))
+                return interviewer;
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "\nID: " + this.id +
+                "\nName: " + this.name +
+                "\nLast Name: " + this.lastName +
+                "\nEmail: " + this.email +
+                "\nIs Active: " + this.isActive + "\n";
+    }
+}
